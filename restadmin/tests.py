0e07b34307a8e31962273ea99b26db9e05279a39
Victor Aderibigbe <victoraderibigbe03@gmail.com>
2022-06-01 23:38:51 +0100

    tests

--- restadmin/tests.py
+++ restadmin/tests.py
@@ -1,7 +1,8 @@
 from rest_framework.test import APITestCase
 from rest_framework.viewsets import ModelViewSet
-from restadmin.models import TestModel
-from restadmin.sites import AdminSite
+from restadmin.models import TestModel, TestAbstractModel
+from restadmin.sites import AdminSite, AlreadyRegistered
+from django.core.exceptions import ImproperlyConfigured
 
 
 # Create your tests here.
@@ -18,3 +19,15 @@ class TestRegistration(APITestCase):
         # self.site.unregister(TestModel)
         # self.assertEqual(self.site._registry, {})
 
+    def test_prevent_double_registration(self):
+        self.site.register(TestModel)
+        with self.assertRaises(AlreadyRegistered):
+            self.site.register(TestModel)
+
+    def test_get_registry(self):
+        self.site.register(TestModel)
+        self.assertEqual(self.site._registry, self.site.get_registry())
+
+    def test_abstract_model_registration(self):
+        with self.assertRaises(ImproperlyConfigured):
+            self.site.register(TestAbstractModel)
